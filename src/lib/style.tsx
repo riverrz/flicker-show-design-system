6f8625180cdcdf251f295a8d14a6b2e0924ad5f8
shivam123425 <shivam123425@gmail.com>
2022-08-14 22:53:25 +0530

    Add stitches css-in-js lib

---
+++ src/lib/style.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { createStitches } from '@stitches/react';
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      primary: '#db0000',
+      secondry: '#831010',
+      black: '#000',
+      white: '#fff',
+      gray: '#333',
+      blue: '#0071eb'
+    },
+  },
+  media: {
+    xs: '(max-width: 480px)',
+    sm: '(min-width: 640px)',
+    md: '(min-width: 768px)',
+    lg: '(min-width: 1024px)',
+  },
+  utils: {
+    marginX: (value: number | string) => ({
+      marginLeft: value,
+      marginRight: value,
+    }),
+    marginY: (value: number | string) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+    paddingX: (value: number | string) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    paddingY: (value: number | string) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
+  },
+});
+
+export const normalizeCSS = () =>
+  globalCss({
+    '*': { margin: 0, padding: 0, boxSizing: 'border-box' },
+    html: { width: '100%', height: '100%' },
+    body: { width: '100%', height: '100%' },
+  });
+
+export const SSRHydrate = () => {
+  return (
+    <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+  );
+};
