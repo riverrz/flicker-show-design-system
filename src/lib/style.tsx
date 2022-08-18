86db37d2f214e09d2bbdc9a7cd241f601accc53f
shivam123425 <shivam123425@gmail.com>
2022-08-18 21:36:11 +0530

    Create space tokens

--- src/lib/style.tsx
+++ src/lib/style.tsx
@@ -17,7 +17,13 @@ export const {
       black: '#000',
       white: '#fff',
       gray: '#333',
-      blue: '#0071eb'
+      blue: '#0071eb',
+    },
+    space: {
+      1: '4px',
+      2: '8px',
+      3: '12px',
+      4: '16px',
     },
   },
   media: {
